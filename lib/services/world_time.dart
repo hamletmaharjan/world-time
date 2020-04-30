d9e9f3a2e6b7b33fdd5cd10e7a09d4f02226e87a
hamlet <imhamlet.hams@gmail.com>
2020-05-01 03:00:00 +0545

    error handle

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,21 +11,28 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async{
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
-    
+    try {
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      
 
-    //crete datetime object
-    String datetime = data['datetime'];
-    String hour = data['utc_offset'].substring(1,3);
-    String minutes = data['utc_offset'].substring(4,6);
+      //crete datetime object
+      String datetime = data['datetime'];
+      String hour = data['utc_offset'].substring(1,3);
+      String minutes = data['utc_offset'].substring(4,6);
 
-    //print(minutes);
-    //print(datetime);
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(hour),minutes: int.parse(minutes)));
+      //print(minutes);
+      //print(datetime);
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(hour),minutes: int.parse(minutes)));
+      
+      time = now.toString();
+    }
+    catch(e) {
+      print('caught error: $e');
+      time = 'Error Loading Time';
+    }
     
-    time = now.toString();
   }
 
   
